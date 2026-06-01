3e51cbc14f15277af18afc065f8bdbf488212e94
Tyler <209157553+thall34@users.noreply.github.com>
2026-06-01 11:51:28 -0400

    Start CSS styling

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const errorHandler = require('./middleware/errorHandler');
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/coaster', coasterRouter);
