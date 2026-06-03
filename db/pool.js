e36527412e4e64fb1fcf829bf7cea947c082fde4
Tyler <209157553+thall34@users.noreply.github.com>
2026-06-03 07:53:00 -0400

    Made change to pool for Render

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,6 @@
 const { Pool } = require('pg');
 
 module.exports = new Pool({
-    host: 'localhost',
-    user: process.env.ROLE_NAME,
-    database: 'roller_coaster_database',
-    password: process.env.ROLE_PASSWORD,
-    port: 5432
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
 });
